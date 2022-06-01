4a7d225ccc025e4cdd1855c448c56aac7cfea319
jamesleiner <59890482+jamesleiner@users.noreply.github.com>
2022-05-31 21:42:57 -0400

    update bash scripts

--- bash/interactive_experiments_batch.sh
+++ bash/interactive_experiments_batch.sh
@@ -7,7 +7,7 @@ while read type tau alt null grid times; do
     echo "#SBATCH -p RM" >> $filename
     echo "#SBATCH -N 1" >> $filename
     echo "#SBATCH -n 128" >> $filename
-    echo "#SBATCH --mem 5GB" >> $filename
+    echo "#SBATCH --mem 20GB" >> $filename
     echo "#SBATCH -t 36:00:00" >> $filename
     echo "#SBATCH --mail-type=ALL" >> $filename
     echo "Rscript interactive_testing_simulations.R "$type $tau $alt $null $grid $times >> $filename
